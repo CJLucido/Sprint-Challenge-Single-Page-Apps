9799feafe37fcc308734a7d6bdf7d313eaf1b73d
CJ Lucido <c.jose.lucido@gmail.com>
2019-10-18 08:27:11 -0700

    installed all dependencies, started adding components to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+
+
 import Header from "./components/Header.js";
+import SearchForm from "./components/SearchForm.js"
+import WelcomePage from "./components/WelcomePage.js"
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <SearchForm/>
+      <WelcomePage/>
+
     </main>
   );
 }
